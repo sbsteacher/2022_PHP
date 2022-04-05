be6fa0a6718727c930039718e3c1663d173533e1
박도흠 <pirbak@gmail.com>
2022-04-05 18:14:07 +0900

    220405

---
+++ ch03/arithmetic.php
@@ -0,0 +1,19 @@
+<?php
+    $n1 = 8;
+    $n2 = 5;
+
+    $result = $n1 + $n2;
+    print "$n1 + $n2 = $result <br>";
+
+    $result = $n1 - $n2;
+    print "$n1 - $n2 = $result <br>";
+
+    $result = $n1 * $n2;
+    print "$n1 * $n2 = $result <br>";
+
+    $result = $n1 / $n2;
+    print "$n1 / $n2 = $result <br>";
+
+    $result = $n1 % $n2; //modulo
+    print "$n1 % $n2 = $result <br>";
+?>
---
+++ ch03/print.php
@@ -0,0 +1,6 @@
+<?php
+    print ("<div>Hello PHP</div>");
+    print "<div class='cls1'>Hello PHP</div>";    
+?>
+
+
