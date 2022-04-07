ecf929792cf3ba33f3e3d453e450de0822ed6e55
박도흠 <pirbak@gmail.com>
2022-04-07 12:12:28 +0900

    220407_오전

--- ch04/for_mission4.php
+++ ch04/for_mission4.php
@@ -3,17 +3,17 @@
         지금까지 배운 것 모두 활용하여
         출력: [1, 2, 3, 4, 5, 6, 7]
     */
-    $end_val = 10;
+    $end_val = 12;
     echo "[";
-    for($i=1; $i<=$end_val; $i++)
+    for($i=1; $i<$end_val; $i++)
     {
         print $i;
-        if($i < $end_val) { print ", "; }
+        if($i < $end_val-1) { print ", "; }
     }
     echo "]";
     echo "<br>";
     echo "[";
-    for($i=1; $i<=$end_val; $i++)
+    for($i=1; $i<$end_val; $i++)
     {
         if($i > 1) { print ", "; }
         print $i;
