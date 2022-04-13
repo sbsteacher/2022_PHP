a0360c08363f6a060294d2ad45ef69c479f40284
sbsteacher <pirbak@gmail.com>
2022-04-13 15:00:28 +0900

    dynamic_parameter

--- ch06/dynamic_parameter.php
+++ ch06/dynamic_parameter.php
@@ -2,19 +2,21 @@
 
     function print_sum(...$vals)
     {
-        $sum = 0;
+        /*
         for($i=0; $i<count($vals); $i++)
         {
             $val = $vals[$i];
             $sum += $val;
+            //$sum += $vals[$i];
         }
+        */
         /*
         foreach($vals as $val)
         {
             $sum += $val;
         }
         */
-        print "sum : $sum <br>";
+        print "sum : " . array_sum($vals) . "<br>";
     }
 
     print_sum(1, 2);       // sum: 3
