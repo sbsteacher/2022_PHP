d630ee64440c5ebe5b0c54a8b4d5780ac04819fa
박도흠 <pirbak@gmail.com>
2022-04-27 17:30:16 +0900

    페이징 처리 완료

--- board_login/db/db_board.php
+++ board_login/db/db_board.php
@@ -19,13 +19,27 @@
         return $result;
     }
 
-    function sel_board_list() {
+    function sel_paging_count(&$param) {
+        $row_count = $param["row_count"];
+        $sql = "SELECT CEIL(COUNT(i_board) / $row_count) as cnt
+                  FROM t_board";
+        $conn = get_conn();
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn); 
+        $row = mysqli_fetch_assoc($result);
+        return $row["cnt"];
+    }
+
+    function sel_board_list(&$param) {
+        $start_idx = $param["start_idx"];
+        $row_count = $param["row_count"];
         $sql = "SELECT A.i_board, A.title, A.created_at
                      , B.nm
                   FROM t_board A
                  INNER JOIN t_user B
                     ON A.i_user = B.i_user
-                 ORDER BY A.i_board DESC";
+                 ORDER BY A.i_board DESC
+                 LIMIT $start_idx, $row_count";
         $conn = get_conn();
         $result = mysqli_query($conn, $sql);
         mysqli_close($conn);
--- board_login/list.php
+++ board_login/list.php
@@ -3,11 +3,24 @@
 
     session_start();
     $nm = "";
+    $page = $_GET["page"];
+    if(!$page) { 
+        $page = 1; 
+    } else {
+        $page = intval($page);
+    }
+    print "page : " . $page;
     if(isset($_SESSION["login_user"])) {
         $login_user = $_SESSION["login_user"];
         $nm = $login_user["nm"];
     }
-    $list = sel_board_list();
+    $row_count = 20;
+    $param = [
+        "row_count" => $row_count,
+        "start_idx" => ($page - 1) * $row_count
+    ];
+    $paging_count = sel_paging_count($param);
+    $list = sel_board_list($param);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,6 +67,11 @@
                     <?php } ?>
                 </tbody>
             </table>
+            <div>
+            <?php for($i=1; $i<=$paging_count; $i++) { ?>
+                <span><a href="list.php?page=<?=$i?>"><?=$i?></a></span>                    
+            <?php } ?>
+            </div>
         </main>
     </div>
 </body>
