2cb2a5dab45bca763dedcdaf129d8fa8862ec6da
박도흠 <pirbak@gmail.com>
2022-05-19 21:04:57 +0900

    중복된 값 없는 랜덤숫자 뽑아내기

---
+++ ch05/array_mission_1_1.php
@@ -0,0 +1,19 @@
+<?php
+    $arr = [4, 9, 10, 22, 100, 45, 99, 11];
+
+    for($i=count($arr)-1; $i>=0; $i--) {
+        print $arr[$i] . "<br>";
+    }
+
+    print "---------------<br>";
+
+    $len = count($arr);
+    for($i=1; $i<=$len; $i++) {
+        print $arr[$len - $i] . "<br>";
+    }
+
+    $len = count($arr);
+    for($i=1; $i<=$len; $i++){
+        $d = $len - $i; 
+        print $arr[$d] . "<br>";
+    }
