317794a1dcecb9702ea9f12edc9c2af64e6a1ff4
박도흠 <pirbak@gmail.com>
2022-05-24 17:20:11 +0900

    부트스트랩

---
+++ bootstrap/picture.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <picture>
+        <source media="(min-width: 700px)" srcset="https://i.picsum.photos/id/1052/900/500.jpg?hmac=ftPgt4Bl5cjdC3v6gaNmIALylCPLs1e_-lx5duokucs">
+        <source media="(min-width: 400px)" srcset="https://i.picsum.photos/id/13/500/300.jpg?hmac=wOBu-PTe-XaqcP3dewVwSDf_lr2eFWzePY5VvVYpaKE">
+        <img src="https://i.picsum.photos/id/657/300/180.jpg?hmac=rwdJmjgU1drn8yKHuDG6kJ7UCJO6GC43xNArb9rRtFo" alt="People">
+    </picture>
+</body>
+</html>
